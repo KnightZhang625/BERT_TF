a828b0582fa8b303c96512a90c5502defa39d685
KnightZhang <knight.625@live.com>
2019-10-17 22:42:00 +0800

    tutorial_3 gives a good demonstrate, need to fix update bug

--- tutorial/tutorial_3.py
+++ tutorial/tutorial_3.py
@@ -3,7 +3,7 @@
 import functools
 import numpy as np
 import tensorflow as tf
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 
 import sys
 import logging
@@ -23,9 +23,8 @@ handlers = [
 logging.getLogger('tensorflow').handlers = handlers
 
 class Model(object):
-    def __init__(self, input_tensor):
-        print(input_tensor)
-        self.output = tf.layers.dense(input_tensor, 1)
+    def __init__(self, x, b):
+        self.output = tf.add(tf.layers.dense(x, 1), b)
     
     def get_output(self):
         return self.output
@@ -34,41 +33,48 @@ class Model(object):
 def model_fn(features, labels, mode, params):
     """this is prototype syntax, all parameters are necessary."""
     if isinstance(features, dict):
-        x = features['x_1']
-        x_2 = features['x_2']
+        x = features['x']
+        b = features['b']
         labels = features['y']
 
-    # input_tensor = tf.concat((x_1, x_2), axis=-1)
-    # model = Model(input_tensor)
-    # predictions = model.get_output()
- 
-    predictions = tf.layers.dense(x, 1)
+    batch_size = tf.cast(params['batch_size'], tf.float32)
+    model = Model(x, b)
+    outputs = model.get_output()
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+        return tf.estimator.EstimatorSpec(mode, predictions=outputs)
     else:
-        loss = tf.nn.l2_loss(predictions - labels)
+        loss = tf.nn.l2_loss(outputs - labels) / batch_size
         if mode == tf.estimator.ModeKeys.EVAL:
             return tf.estimator.EstimatorSpec(mode, loss=loss)
         elif mode == tf.estimator.ModeKeys.TRAIN:
+            # optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+            # parameters = tf.trainable_variables()
+            # gradients = tf.gradients(loss, parameters, colocate_gradients_with_ops=True)
+            # clipped_gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+            # train_op = optimizer.apply_gradients(zip(clipped_gradients, parameters))
             train_op = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss, global_step=tf.train.get_global_step())
             return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
         else:
             raise NotImplementedError()
 
-def train_generator_fn(X_1, X_2, Y):
-    for (x_1, x_2, y) in zip(X_1, X_2, Y):
-        features = {'x_1': x_1, 'x_2': x_2, 'y': y}
+def train_generator_fn(xs, bs, ys):
+    """This is the entrance to the input_fn."""
+    for (x, b, y) in zip(xs, bs, ys):
+        # the key in the following dictionary
+        # should be identical to the key of the output_types and output_shapes
+        # in the train_input_fn.
+        features = {'x': x, 'b': b, 'y': y}
         yield features
 
-def train_input_fn(X_1, X_2, Y):
-    output_types = {'x_1': tf.float32, 'x_2': tf.float32, 'y': tf.float32}
-    output_shapes = {'x_1': (2), 'x_2': (1), 'y': (1)}
+def train_input_fn(xs, bs, ys):
+    output_types = {'x': tf.float32, 'b': tf.float32, 'y': tf.float32}
+    output_shapes = {'x': (2), 'b': (1), 'y': (1)}
     dataset = tf.data.Dataset.from_generator(
-       functools.partial(train_generator_fn, X_1, X_2, Y),
+       functools.partial(train_generator_fn, xs, bs, ys),
        output_types=output_types,
        output_shapes=output_shapes)
-    dataset = dataset.batch(5).repeat(1000)
+    dataset = dataset.batch(2).repeat(1000)
     return dataset
 
 # def my_service():
@@ -76,16 +82,21 @@ def train_input_fn(X_1, X_2, Y):
 #         yield number
 
 def serving_input_receiver_fn():
-    x = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='number')
-    receiver_tensors = {'user_input': x}
-    features = {'x_1': x, 'x_2': tf.zeros(0), 'y': tf.zeros(0)}
+    x = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='x')
+    b = tf.placeholder(dtype=tf.float32, shape=[1], name='b')
+    receiver_tensors = {'x': x, 'b': b}
+    features = {'x': x, 'b': b, 'y': tf.zeros(0)}
     return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
 
 if __name__ == '__main__':
     # create some data, the function is f(x,y) = x + y
-    x_1 = np.array([[i, i] for i in range(100)], dtype=np.float)               # (100, 3)
-    x_2 = np.array([[i] for i in range(100, 200)], dtype=np.float)          # (100, 1)
-    y = np.array([[2 * x[0]] for x in x_1], dtype=np.float)       # (100, )
+    xs = [[2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]
+    bs = [[0.1] for _ in range(5)]
+    ys = [[x[0] + 2 * x[1] + b[0]]for x, b in zip(xs, bs)]
+    
+    xs = np.array(xs, dtype=np.float)   # (5, 2)
+    bs = np.array(bs, dtype=np.float)   # (5, 1)
+    ys = np.array(ys, dtype=np.float)     # (5, 1)
     
     # # eager mode test
     # for features in train_input_fn(x_1, x_2, y):
@@ -94,15 +105,15 @@ if __name__ == '__main__':
     #     print(features['y'])
     #     input()
 
-    # input_fn = functools.partial(train_input_fn, X_1=x_1, X_2=x_2, Y=y)
-    # estimator = tf.estimator.Estimator(model_fn, 'model_3', params={})
+    # input_fn = functools.partial(train_input_fn, xs=xs, bs=bs, ys=ys)
+    # estimator = tf.estimator.Estimator(model_fn, 'model_3_1', params={'batch_size':1})
     # estimator.train(input_fn)
 
 
-    # estimator = tf.estimator.Estimator(model_fn, 'model_3', params={})
-    # estimator.export_saved_model('saved_model_3', serving_input_receiver_fn)
+    # estimator = tf.estimator.Estimator(model_fn, 'model_3_1', params={'batch_size':2})
+    # estimator.export_saved_model('saved_model_3_1', serving_input_receiver_fn)
 
-    export_dir = 'saved_model_3'
+    export_dir = 'saved_model_3_1'
     subdirs = [x for x in Path(export_dir).iterdir()
             if x.is_dir() and 'temp' not in str(x)]
     latest = str(sorted(subdirs)[-1])
@@ -110,5 +121,7 @@ if __name__ == '__main__':
     from tensorflow.contrib import predictor
     predict_fn = predictor.from_saved_model(latest)
     # for nb in my_service():
-    pred = predict_fn({'user_input': np.array([[2, 2]], dtype=np.float)})['output']
+    i_x = np.array([[2, 3]], dtype=np.float)
+    i_b = np.array([0.1], dtype=np.float)
+    pred = predict_fn({'x': i_x, 'b': i_b})['output']
     print(pred)
